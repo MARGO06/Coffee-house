566d30bff55ca9c65117b04f8c180450914424f4
Margarita Masko <margaritam996@gmail.com>
2024-02-25 23:42:32 +0300

    refactor: add utility types like NonNullable

--- news-api/src/components/view/news/news.ts
+++ news-api/src/components/view/news/news.ts
@@ -1,6 +1,8 @@
 import './news.css';
 import { ArticlesNew } from '../../../types/types';
 
+export type ElementDom = NonNullable<HTMLElement | null | undefined>;
+
 class News {
     draw(data: ArticlesNew[]) {
         const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
@@ -15,34 +17,22 @@ class News {
             if (idx % 2 && newsItem !== null) newsItem.classList.add('alt');
 
             const newsPhoto = <HTMLDivElement>newsClone.querySelector('.news__meta-photo');
-            const newsAuthor = newsClone.querySelector('.news__meta-author');
-            const newsDate = newsClone.querySelector('.news__meta-date');
-            const newsTitle = newsClone.querySelector('.news__description-title');
-            const newsSource = newsClone.querySelector('.news__description-source');
-            const newsContent = newsClone.querySelector('.news__description-content');
-            const newsRead = newsClone.querySelector('.news__read-more a');
+            const newsAuthor: ElementDom = newsClone.querySelector('.news__meta-author')!;
+            const newsDate: ElementDom = newsClone.querySelector('.news__meta-date')!;
+            const newsTitle: ElementDom = newsClone.querySelector('.news__description-title')!;
+            const newsSource: ElementDom = newsClone.querySelector('.news__description-source')!;
+            const newsContent: ElementDom = newsClone.querySelector('.news__description-content')!;
+            const newsRead: ElementDom = newsClone.querySelector('.news__read-more a')!;
 
             if (newsPhoto !== null) {
                 newsPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
             }
-            if (newsAuthor !== null) {
-                newsAuthor.textContent = item.author || item.source.name;
-            }
-            if (newsDate !== null) {
-                newsDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
-            }
-            if (newsTitle !== null) {
-                newsTitle.textContent = item.title;
-            }
-            if (newsSource !== null) {
-                newsSource.textContent = item.source.name;
-            }
-            if (newsContent !== null) {
-                newsContent.textContent = item.description;
-            }
-            if (newsRead !== null) {
-                newsRead.setAttribute('href', item.url);
-            }
+            newsAuthor.textContent = item.author || item.source.name;
+            newsDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-');
+            newsTitle.textContent = item.title;
+            newsSource.textContent = item.source.name;
+            newsContent.textContent = item.description;
+            newsRead.setAttribute('href', item.url);
             fragment.append(newsClone);
         });
         const containerNews = <HTMLDivElement>document.querySelector('.news');
