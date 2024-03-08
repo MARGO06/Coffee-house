e514d3e5a5d2499c9552834c4363be106a70eb2b
Margarita Masko <margaritam996@gmail.com>
2024-03-08 22:47:46 +0300

    feat: add pre-commit and pre-push

--- rss-puzzle/src/script.ts
+++ rss-puzzle/src/script.ts
@@ -1,6 +1,8 @@
 import './style.css';
-// import pexel from './assets/pexel.jpg';
+//import pexel from './assets/pexel.jpg';
 console.log('hello');
+// test committttt
+// test 2
 function sum(a: number, b: number) {
   console.log(a + b);
 }
