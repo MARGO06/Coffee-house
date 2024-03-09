ab97f49821b57780ad33b361b6c919bef9c478e1
Margarita Masko <margaritam996@gmail.com>
2024-03-09 16:30:14 +0300

    feat: add two files in husky folder and add noImplisityAny in tsconfig

--- rss-puzzle/src/script.ts
+++ rss-puzzle/src/script.ts
@@ -1,9 +1,16 @@
 import './style.css';
-//import pexel from './assets/pexel.jpg';
+import pexel from './assets/pexel.jpg';
 console.log('hello');
 // test committttt
-// test 2
+// test 4
+//test 5
 function sum(a: number, b: number) {
   console.log(a + b);
 }
-sum(8, 9);
+sum(5, 10);
+
+function summ(a: string, b: string) {
+  console.log(a.concat(b));
+  console.log(pexel);
+}
+summ('hel', 'lo');
