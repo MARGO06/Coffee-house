72a77f0fee89864037d857acc3832229fcbc39bb
MARGO06 <118379169+MARGO06@users.noreply.github.com>
2024-03-15 11:22:01 +0300

    feat: add logout button and check function between pages (#38)

--- rss-puzzle/src/script.ts
+++ rss-puzzle/src/script.ts
@@ -1,6 +1,7 @@
 import { AllPages } from './pages/all-pages';
-import { SaveElements } from './functions/login-pages-action';
 import './style.css';
 
-AllPages.renderPage('login');
-SaveElements.saveLocalStorage();
+const page = new AllPages();
+page.createLogin();
+
+page.createMain();
