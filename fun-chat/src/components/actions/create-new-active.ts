060f6c848b79964a28b85e6ad74fff8715ca09c1
Margarita Masko <margaritam996@gmail.com>
2024-04-16 17:22:32 +0300

    feat: add change status to inactive from active whith current user

--- fun-chat/src/components/actions/create-new-active.ts
+++ fun-chat/src/components/actions/create-new-active.ts
@@ -2,8 +2,11 @@ import { getNewActive } from '../websocket/user_external';
 
 export const newActive = async () => {
   const active = document.querySelector('.active');
+  const inactive = document.querySelectorAll('.users_inactive');
+  const nameDestination = document.querySelector('.main_destination-name');
+  const status = document.querySelector('.main_destination-status');
   const answer = await getNewActive();
-  if (answer.type === 'USER_EXTERNAL_LOGIN') {
+  if (answer.type === 'USER_EXTERNAL_LOGIN' || answer.type === 'USER_ACTIVE') {
     const users = answer.payload.user.login;
     const userActive = document.createElement('li');
     userActive.className = 'users_active';
@@ -11,20 +14,28 @@ export const newActive = async () => {
     const countMessages = document.createElement('div');
     countMessages.className = `count ${users}`;
     countMessages.dataset.count = `0`;
+    inactive.forEach((name) => {
+      if (name.innerHTML === users) {
+        console.log(name.innerHTML);
+        name.remove();
+        deleteCount(users);
+      }
+    });
     userActive.addEventListener('click', (e) => {
-      const nameDestination = document.querySelector('.main_destination-name');
-      const status = document.querySelector('.main_destination-status');
       e.preventDefault();
       if (nameDestination instanceof HTMLElement && status instanceof HTMLElement) {
         nameDestination.innerHTML = users;
         status.innerHTML = 'active';
         status.style.color = 'rgb(27, 243, 8)';
       }
+      newActive();
+      exitNewActive();
     });
 
     if (active instanceof HTMLUListElement) active.append(userActive, countMessages);
   }
   exitNewActive();
+
   return active;
 };
 
@@ -32,13 +43,44 @@ export async function exitNewActive() {
   const answer = await getNewActive();
   if (answer.type === 'USER_EXTERNAL_LOGOUT' || answer.type === 'USER_LOGOUT') {
     const logout = document.querySelectorAll('.users_active');
+    const inactive = document.querySelector('.inactive');
     const users = answer.payload.user.login;
-    console.log(users, logout);
     logout.forEach((name) => {
       if (name.innerHTML === users) {
         name.remove();
+        const userActive = document.createElement('li');
+        userActive.className = 'users_inactive';
+        userActive.textContent = users;
+        const countMessages = document.createElement('div');
+        countMessages.className = `count ${users}`;
+        countMessages.dataset.count = `0`;
+        deleteCount(name.innerHTML);
+        changeStatus(name.innerHTML);
+        if (inactive instanceof HTMLElement) inactive.append(userActive, countMessages);
       }
     });
+
     newActive();
+  }
+}
+
+function deleteCount(data: string) {
+  const counts = document.querySelectorAll('.count');
+  counts.forEach((count) => {
+    if (count.classList.contains(data)) {
+      console.log(count);
+      count.remove();
+    }
+  });
+}
+
+function changeStatus(data: string) {
+  const nameDestination = document.querySelector('.main_destination-name');
+  const status = document.querySelector('.main_destination-status');
+  if (nameDestination instanceof HTMLElement && status instanceof HTMLElement) {
+    if (nameDestination.innerHTML === data) {
+      status.innerHTML = 'inactive';
+      status.style.color = 'rgba(194, 6, 6, 0.847)';
+    }
   }
 }
