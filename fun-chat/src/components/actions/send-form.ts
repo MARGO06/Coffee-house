aeb2346c9670617e824da49beabcade92b59b143
Margarita Masko <margaritam996@gmail.com>
2024-04-19 17:21:21 +0300

    fix: change to send a form to pressing Enter button

--- fun-chat/src/components/actions/send-form.ts
+++ fun-chat/src/components/actions/send-form.ts
@@ -7,27 +7,32 @@ export const sendForm = () => {
   const passwordInput = document.getElementById('password') as HTMLInputElement;
   form.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-      event.preventDefault();
-      form.dispatchEvent(new Event('submit'));
-      if (nameInput.validity.valid && passwordInput.validity.valid) {
-        localStorage.setItem('first name', nameInput.value);
-        localStorage.setItem('password', passwordInput.value);
-        const data = {
-          id: '1',
-          type: 'USER_LOGIN',
-          payload: {
-            user: {
-              login: `${nameInput.value}`,
-              password: `${passwordInput.value}`,
+      if (!nameInput.value && !passwordInput.value) {
+        event.preventDefault();
+      }
+      if (nameInput.value && passwordInput.value) {
+        event.preventDefault();
+        form.dispatchEvent(new Event('submit'));
+        if (nameInput.validity.valid && passwordInput.validity.valid) {
+          localStorage.setItem('first name', nameInput.value);
+          localStorage.setItem('password', passwordInput.value);
+          const data = {
+            id: '1',
+            type: 'USER_LOGIN',
+            payload: {
+              user: {
+                login: `${nameInput.value}`,
+                password: `${passwordInput.value}`,
+              },
             },
-          },
-        };
-        socket.send(JSON.stringify(data));
-        nameInput.value = '';
-        passwordInput.value = '';
-        const window = new AllPages();
-        window.showModalWindows();
-        window.backToLoginPage();
+          };
+          socket.send(JSON.stringify(data));
+          nameInput.value = '';
+          passwordInput.value = '';
+          const window = new AllPages();
+          window.showModalWindows();
+          window.backToLoginPage();
+        }
       }
     }
   });
